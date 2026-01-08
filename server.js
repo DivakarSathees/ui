5831bf3445882990ff7833642fa4478acf5c117c
DivakarSathees <divakarsathees7@gmail.com>
2026-01-08 19:03:51 +0530

    asd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4200;
+const port = process.env.PORT || 4200;
 
 //app.use('/static', express.static('public'))
 app.use(express.static('public'))
